5cf992fd683a89039751adadd4871d406f504bfe
kanta <kanta.mo.ac@gmail.com>
2020-04-22 15:39:49 +0900

    OpenGLで描画成功　リアルタイムの取得はまだ

--- mission14.cpp
+++ mission14.cpp
@@ -8,7 +8,10 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtx/string_cast.hpp>
+#include <glm/gtc/type_ptr.hpp>
 #include "shader.hpp"
+#include "objloader.hpp"
+
 
 using namespace std;
 using namespace cv;
@@ -56,7 +59,7 @@ int main(int argc, char* argv[]) {
 	for (i = 0; i < 10; i++) {
 		auto found = findChessboardCorners(srcimg[i], pattern_size, corners);
 		if (found) {
-			cout << i << "...ok" << endl;
+			//cout << i << "...ok" << endl;
 			found_num++;
 		}
 		else {
@@ -101,75 +104,226 @@ int main(int argc, char* argv[]) {
 		tvecs2
 	);
 
+	//imshow("opencv",srcimg[0]);
+	//回転行列(3*3)
+	Mat rvecs_mat;
+	//回転ベクトルを回転行列に変換
+	Rodrigues(rvecs2, rvecs_mat);
+	Mat tvecs_nom = tvecs2 / 17.0;	//チェスボードの大きさで割る
 
-	/*
-	GLuint programID = LoadShaders("SimpleTransform.vertexshader","SingleColor.fragmentshader");
-	GLuint MatrixID = glGetUniformLocation(programID, "MVP");
+	Mat view_cv = Mat::zeros(4,4,CV_64F);
+	view_cv.at<double>(0, 0) = rvecs_mat.at<double>(0, 0);	view_cv.at<double>(0, 1) = rvecs_mat.at<double>(0, 1);	view_cv.at<double>(0, 2) = rvecs_mat.at<double>(0, 2); view_cv.at<double>(0, 3) = tvecs_nom.at<double>(0);
+	view_cv.at<double>(1, 0) = rvecs_mat.at<double>(1, 0);	view_cv.at<double>(1, 1) = rvecs_mat.at<double>(1, 1);	view_cv.at<double>(1, 2) = rvecs_mat.at<double>(1, 2); view_cv.at<double>(1, 3) = tvecs_nom.at<double>(1);
+	view_cv.at<double>(2, 0) = rvecs_mat.at<double>(2, 0);	view_cv.at<double>(2, 1) = rvecs_mat.at<double>(2, 1);	view_cv.at<double>(2, 2) = rvecs_mat.at<double>(2, 2); view_cv.at<double>(2, 3) = tvecs_nom.at<double>(2);
+	view_cv.at<double>(3, 3) = 1;
 
-	//プロジェクション行列
-	mat4 Projection;
-	cameraFrustumRH(cam_mat, srcimg[0],Projection,0.1,5000);
+	Mat Rot_x = Mat::zeros(4, 4, CV_64F);
+	Rot_x.at<double>(0, 0) = 1; Rot_x.at<double>(1, 1) = -1; Rot_x.at<double>(2, 2) = -1; Rot_x.at<double>(3, 3) = 1;
+	view_cv= Rot_x * view_cv;
 
-	//カメラビュー行列
-	mat4 View = mat4(1.0)
-				*lookAt(
-					vec3(0, 0, 0),	//カメラの位置
-					vec3(0, 0, 1),	//原点を見ている
-					vec3(0,-1, 0)	//カメラの上方向
-				);
+	transpose(view_cv, view_cv);
 
-	//モデル行列
-	mat4 Model = mat4(1.0f);
-
-	mat4 MVP = Projection * View * Model;
-	/*FileStorage fs("PV", cv::FileStorage::WRITE);
+	/*FileStorage fs("external parameter.xml", cv::FileStorage::WRITE);
 	if (!fs.isOpened())
 	{
 		cerr << "File can not be opened." << endl;
 		return -1;
 	}
 
-	fs << "プロジェクション行列" << Projection;
-	fs << "カメラビュー行列" << View;
-	fs.release();*/
-
-	//cout << to_string(View);
-
-	while (1) {
-		glutInit(&argc, argv);
-		glutInitDisplayMode(GLUT_RGBA);
-		glutInitWindowSize(srcimg[0].cols, srcimg[0].rows);
-		glutCreateWindow("marker capture");
-		glMatrixMode(GL_PROJECTION);
-		glLoadIdentity();
-		gluOrtho2D(-1, 1, -1, 1);
-		glViewport(-1, -1, srcimg[0].cols, srcimg[0].rows);
-
-		flip(srcimg[0], srcimg[0], 0);
-		cvtColor(srcimg[0], srcimg[0], COLOR_BGR2RGB);
-
-		glClear(GL_COLOR_BUFFER_BIT);
-		glDrawPixels(srcimg[0].cols, srcimg[0].rows, GL_RGB, GL_UNSIGNED_BYTE, srcimg[0].data);
-		glFlush();
-
-		glutMainLoop();
+	fs << "rotation" << rvecs2;
+	fs << "translation" << tvecs2;
+	fs.release();
+	//cout << tvecs2/17.0;*/
+	
+	//GLFWの初期化
+	if (!glfwInit()) {
+		cerr << "Failed to initialize GLFW\n";
+		return -1;
 	}
 
+	glfwWindowHint(GLFW_SAMPLES, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	//ウィンドウを開く
+	GLFWwindow* window = glfwCreateWindow(srcimg[0].cols, srcimg[0].rows, "Marker capture",NULL,NULL);
+	if (window == NULL) {
+		fprintf(stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n");
+		getchar();
+		glfwTerminate();
+		return -1;
+	}
+	//レンダリングを行うウィンドウを指定する
+	glfwMakeContextCurrent(window);
+
+	glewExperimental = true;
+	if (glewInit() != GLEW_OK) {
+		std::cerr << "Failed to initialize GLEW.\n";
+		return -1;
+	}
+	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+
+	GLuint Background_VertexArrayID;	//頂点配列オブジェクト(VAO)
+	glGenVertexArrays(1, &Background_VertexArrayID);	//VAO作成
+	glBindVertexArray(Background_VertexArrayID);	//頂点配列オブジェクトを結合
+
+	//モデル用のVAO
+	GLuint Model_VertexArrayID;	
+	glGenVertexArrays(1, &Model_VertexArrayID);
+	glBindVertexArray(Model_VertexArrayID);
+
+	GLuint programID = LoadShaders("background.vert", "background.frag");
+	GLuint model_programID = LoadShaders("model.vert", "model.frag");
+
+	GLuint MatrixID = glGetUniformLocation(model_programID, "MVP");
+	//プロジェクション行列
+	mat4 Projection;
+	cameraFrustumRH(cam_mat, srcimg[0], Projection, 0.1, 500);
+
+	//mat4 Projection = perspective(glm::radians(90.0f), 4.0f / 3.0f, 0.1f, 100.0f);
+
+	mat4 View=make_mat4(reinterpret_cast<GLdouble*>(view_cv.data));
+
+	/*mat4 View = glm::lookAt(
+		glm::vec3(4, 3, 3), // ワールド空間でカメラは(4,3,3)
+		glm::vec3(0, 0, 0), // 原点を見ている
+		glm::vec3(0, 1, 0)  // 頭が上方向(0,-1,0にセットすると上下逆転する)
+	);*/
+
+
+	mat4 Model = mat4(1.0f);	//単位行列
+	Model[1][2] = 1.0; Model[2][1] = -1.0; Model[1][1] = 0.0; Model[2][2] = 0.0;
+
+	mat4 MVP = Projection * View * Model;
+
+	const GLfloat background_vertices[] = {
+		-1.0f, -1.0f, 0.0f,
+		-1.0f,  1.0f, 0.0f,
+		 1.0f,  1.0f, 0.0f,
+		 1.0f, -1.0f, 0.0f,
+	};
+
+	const GLfloat background_uv[] = {
+		0.0f, 1.0f,
+		0.0f, 0.0f,
+		1.0f, 0.0f,
+		1.0f, 1.0f,
+	};
+
+	GLuint texture;
+	glGenTextures(1, &texture);	//テクスチャオブジェクトを生成
+	glBindTexture(GL_TEXTURE_2D, texture);	//テクスチャの次元を指定、指名したテクスチャを有効にする
+	cvtColor(srcimg[0], srcimg[0], cv::COLOR_BGR2RGBA);
+	//画像データと関連付ける
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, srcimg[0].cols, srcimg[0].rows, 0, GL_RGBA, GL_UNSIGNED_BYTE, srcimg[0].data);
+	//画像を補完する際のパラメータ指定
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+
+	GLuint textureID = glGetUniformLocation(programID, "texture");
+
+	GLuint bg_vertexbuffer;
+	glGenBuffers(1, &bg_vertexbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, bg_vertexbuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(background_vertices), background_vertices, GL_STATIC_DRAW);
+
+	GLuint background_uvbuffer;
+	glGenBuffers(1, &background_uvbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, background_uvbuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(background_uv), background_uv, GL_STATIC_DRAW);
+
+	//objファイルの読み込み
+	vector<vec3> vertices;
+	vector<vec2> uvs;
+	vector<vec3> normals;
+	bool res = loadOBJ("axis.obj", vertices,uvs, normals);
+
+	//VBOに読み込む
+	GLuint model_vertexbuffer;
+	glGenBuffers(1, &model_vertexbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, model_vertexbuffer);
+	glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(vec3), &vertices[0], GL_STATIC_DRAW);
+
+	do {
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glUseProgram(programID);
+	
+
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, texture);
+		glUniform1i(textureID, 0);
+
+		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, bg_vertexbuffer);
+		glVertexAttribPointer(
+			0,
+			3,
+			GL_FLOAT,
+			GL_FALSE,
+			0,
+			(void*)0
+		);
+
+		glEnableVertexAttribArray(1);
+		glBindBuffer(GL_ARRAY_BUFFER, background_uvbuffer);
+		glVertexAttribPointer(
+			1,                                // attribute. No particular reason for 1, but must match the layout in the shader.
+			2,                                // size : U+V => 2
+			GL_FLOAT,                         // type
+			GL_FALSE,                         // normalized?
+			0,                                // stride
+			(void*)0                          // array buffer offset
+		);
+
+		glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
+		glDisableVertexAttribArray(0);
+		glDisableVertexAttribArray(1);
+
+		glUseProgram(model_programID);
+		glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
+
+		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, model_vertexbuffer);
+		glVertexAttribPointer(
+			0,                  // attribute. No particular reason for 0, but must match the layout in the shader.
+			3,                  // size
+			GL_FLOAT,           // type
+			GL_FALSE,           // normalized?
+			0,                  // stride
+			(void*)0            // array buffer offset
+		);
+		glDrawArrays(GL_TRIANGLES, 0, vertices.size());
+
+		glDisableVertexAttribArray(0);
+		glDisableVertexAttribArray(1);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	} while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS && glfwWindowShouldClose(window) == 0);
+
+	glDeleteBuffers(1, &bg_vertexbuffer);
+	glDeleteBuffers(1, &background_uvbuffer);
+	glDeleteProgram(programID);
+	glDeleteTextures(1, &texture);
+	glDeleteVertexArrays(1, &Background_VertexArrayID);
+	glfwTerminate();
 	return 0;
 }
 
+//プロジェクション行列の計算
 void cameraFrustumRH(Mat camMat, Mat srcimg, mat4& projMat, double znear, double zfar) {
 	// Load camera parameters
 	double fx = camMat.at<double>(0, 0);
 	double fy = camMat.at<double>(1, 1);
 	double cx = camMat.at<double>(0, 2);
 	double cy = camMat.at<double>(1, 2);
-	double w = srcimg.rows, h = srcimg.cols;
+	double w = srcimg.cols, h = srcimg.rows;
 
 	mat4 projection(
-		2.0 * fx / w, 0, 0, 0,
-		0, 2.0 * fy / h, 0, 0,
-		1.0 - 2.0 * cx / w, -1.0 + 2.0 * cy / h, -(zfar + znear) / (zfar - znear), -1.0,
-		0, 0, -2.0 * zfar * znear / (zfar - znear), 0);
+		2.0 * fx / w,		 0,					 0,									   0,
+		0,					 2.0 * fy / h,	     0,									   0,
+		1.0 - 2.0 * cx / w, -1.0 + 2.0 * cy / h, -(zfar + znear) / (zfar - znear),	  -1.0,
+		0,					 0,					 -2.0 * zfar * znear / (zfar - znear), 0);
 	projMat = projection;
 }
